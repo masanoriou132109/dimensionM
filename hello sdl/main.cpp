62a1590ea71200d0b1beb79da5305214721bb954
Sherlok Holmes <sunhigh913@gmail.com>
2023-11-14 18:01:44 +0800

    Initial Commit

---
+++ hello sdl/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hello sdl
+//
+//  Created by 王鄭澤 on 2023/11/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
